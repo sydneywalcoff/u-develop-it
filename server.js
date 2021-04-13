c49c1d5879bc3d7236158609c99661af6c399485
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-13 09:54:33 -0700

    first db query call

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '59Zyv@VbJwBJ',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // routes
 app.get('/', (req, res) => {
@@ -13,6 +25,11 @@ app.get('/', (req, res) => {
     });
 });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// catchall route
 app.use((req, res) => {
     res.status(404).end();
 });
